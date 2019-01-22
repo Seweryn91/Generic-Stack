d082904b1614a3ae8fe62f03d1c6f0006d8bca0e
Seweryn91 <s.talaj@gmail.com>
2019-01-22 14:53:21 +0100

    Implement basic constructor

--- src/main/java/GenericStack.java
+++ src/main/java/GenericStack.java
@@ -4,7 +4,15 @@ public class GenericStack<T> {
     private int size;
     private int top;
 
-    public GenericStack(int size) { }
+    public GenericStack(int size) {
+        if (size < 1) {
+            throw new IllegalArgumentException("Stack must be size of 1 or higher!");
+        }
+
+        this.size = size;
+        this.top = -1;
+        this.array = new Object[size];
+    }
 
     public T push(T item){ return item; }
 
