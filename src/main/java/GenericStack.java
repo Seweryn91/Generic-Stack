ae723795c568645f24c8e1a54164c0ac0d3ae557
Seweryn91 <s.talaj@gmail.com>
2019-01-22 14:50:00 +0100

    Declare basic methods

--- src/main/java/GenericStack.java
+++ src/main/java/GenericStack.java
@@ -1,2 +1,16 @@
-public class GenericStack {
+public class GenericStack<T> {
+
+    private Object[] array;
+    private int size;
+    private int top;
+
+    public GenericStack(int size) { }
+
+    public T push(T item){ return item; }
+
+    public T pop(){}
+
+    public int size() { return this.size; }
+
 }
+
