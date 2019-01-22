fd66052501b1a31e3d3bfad12d52d4f7420a8156
Seweryn91 <s.talaj@gmail.com>
2019-01-22 15:08:51 +0100

    Implement StackOverflowException class

---
+++ src/main/java/Exceptions/StackOverflowException.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class StackOverflowException extends Exception {
+
+    public StackOverflowException(String exceptionMessage){
+        super(exceptionMessage);
+    }
+}
