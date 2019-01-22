51c87a3b2f66c92f16b02797bd14f7e2ef5e284f
Seweryn91 <s.talaj@gmail.com>
2019-01-22 15:45:43 +0100

    Add testPush method

--- src/test/java/GenericStackTest.java
+++ src/test/java/GenericStackTest.java
@@ -1,5 +1,28 @@
+import exceptions.StackOverflowException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GenericStackTest {
 
+    @Test
+    @DisplayName("Test initialisation of Generic Stack with improper argument")
+    void testStackInitialisation_wrongArgument() {
+        assertThrows(IllegalArgumentException.class, () -> new GenericStack(0));
+        assertThrows(IllegalArgumentException.class, () -> new GenericStack(-10));
+    }
+
+    @Test
+    @DisplayName("Test pushing elements")
+    void testPush() throws StackOverflowException {
+        GenericStack stack = new GenericStack(10);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        int expectedSize = 3;
+        int actualSize = stack.spaceTaken();
+        assertEquals(expectedSize, actualSize);
+    }
 }
