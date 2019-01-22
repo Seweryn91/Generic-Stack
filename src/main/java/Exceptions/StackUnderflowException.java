a32271b59005d97e64442bb19bccc69bc38d5cb4
Seweryn91 <s.talaj@gmail.com>
2019-01-22 15:12:40 +0100

    Implement StackUnderflowException

---
+++ src/main/java/Exceptions/StackUnderflowException.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class StackUnderflowException extends Exception {
+
+    public StackUnderflowException(String exceptionMessage){
+        super(exceptionMessage);
+    }
+}
