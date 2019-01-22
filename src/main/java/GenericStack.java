e05f04bd2c7988a721122b5ef8667b80121ce2b0
Seweryn91 <s.talaj@gmail.com>
2019-01-22 15:20:34 +0100

    Rename package, remove unnecessary curly brackets

--- src/main/java/GenericStack.java
+++ src/main/java/GenericStack.java
@@ -1,5 +1,5 @@
-import Exceptions.StackOverflowException;
-import Exceptions.StackUnderflowException;
+import exceptions.StackOverflowException;
+import exceptions.StackUnderflowException;
 
 public class GenericStack<T> {
 
@@ -8,9 +8,8 @@ public class GenericStack<T> {
     private int top;
 
     public GenericStack(int size) {
-        if (size < 1) {
+        if (size < 1)
             throw new IllegalArgumentException("Stack must be size of 1 or higher!");
-        }
 
         this.size = size;
         this.top = -1;
@@ -18,9 +17,8 @@ public class GenericStack<T> {
     }
 
     public T push(T item) throws StackOverflowException {
-        if (this.top+1 == this.size) {
+        if (this.top+1 == this.size)
             throw new StackOverflowException("Reached stack size limit!");
-        }
 
         this.array[++this.top] = item;
         return item;
@@ -29,6 +27,7 @@ public class GenericStack<T> {
     public T pop() throws StackUnderflowException {
         if (top == -1)
             throw new StackUnderflowException("Stack is empty!");
+
         Object poppedItem = array[top--];
         return (T) poppedItem;
     }
